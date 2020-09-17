70926c6bb43916bfb6b61327594c054a023eaf86
duointherain <duointherain@gmail.com>
2020-09-17 14:32:33 -0700

    being slow to day

--- index.js
+++ index.js
@@ -39,11 +39,32 @@ class Airplane {
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
+console.log('*** Task One ***');
 
-class Person {
 
+
+class Person{
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+  
+    
+    
+  }
+  job(){
+    return `${this.name} is a ${this.age}`;
+  }
 }
 
+const pilot1 = new Person({
+  name: 'Heero',
+  age: '15',
+  work: 'Computer Programmer',
+});
+
+
+console.log(pilot1.job());
+  
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -58,9 +79,9 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-class Car {
+// class Car {
 
-}
+// }
 
 /*
   TASK 3
@@ -74,9 +95,9 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {
+// class Lambdasian {
 
-}
+// }
 
 /*
   TASK 4
@@ -92,9 +113,9 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+// class Instructor {
 
-}
+// }
 
 /*
   TASK 5
@@ -111,9 +132,9 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+// class Student {
 
-}
+// }
 
 /*
   TASK 6
@@ -128,9 +149,9 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+// class ProjectManager {
 
-}
+// }
 
 /*
   STRETCH PROBLEM (no tests!)
@@ -144,13 +165,13 @@ class ProjectManager {
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Lambdasian) { module.exports.Lambdasian = Lambdasian }
-  if (Instructor) { module.exports.Instructor = Instructor }
-  if (Student) { module.exports.Student = Student }
-  if (ProjectManager) { module.exports.ProjectManager = ProjectManager }
-}
+// if (typeof exports !== 'undefined') {
+//   module.exports = module.exports || {}
+//   if (Airplane) { module.exports.Airplane = Airplane }
+//   if (Person) { module.exports.Person = Person }
+//   if (Car) { module.exports.Car = Car }
+//   if (Lambdasian) { module.exports.Lambdasian = Lambdasian }
+//   if (Instructor) { module.exports.Instructor = Instructor }
+//   if (Student) { module.exports.Student = Student }
+//   if (ProjectManager) { module.exports.ProjectManager = ProjectManager }
+// }
